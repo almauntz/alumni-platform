bae15be5d94a844dfbdb9f3af1dc96b40d79820b
Alma Secerbegovic <alma.secerbegovic@untz.ba>
2026-05-12 21:32:33 +0200

    feat: hardcoded faculty and department data

---
+++ frontend/src/data/faculties.js
@@ -0,0 +1,46 @@
+export const FACULTIES = {
+  "Prirodno-matematički fakultet": [
+    "Matematika", "Fizika", "Hemija", "Biologija", "Geografija", "Ostalo"
+  ],
+  "Filozofski fakultet": [
+    "Bosanski jezik i književnost", "Historija", "Pedagogija", "Psihologija",
+    "Sociologija", "Filozofija", "Komparativna književnost", "Ostalo"
+  ],
+  "Pravni fakultet": [
+    "Pravo", "Kriminalistika", "Ostalo"
+  ],
+  "Ekonomski fakultet": [
+    "Ekonomija", "Menadžment", "Računovodstvo i revizija", "Finansije",
+    "Marketing", "Ostalo"
+  ],
+  "Medicinski fakultet": [
+    "Medicina", "Ostalo"
+  ],
+  "Farmaceutski fakultet": [
+    "Farmacija", "Ostalo"
+  ],
+  "Fakultet elektrotehnike": [
+    "Automatika i elektronika", "Telekomunikacije", "Računarstvo i informatika",
+    "Elektroenergetika", "Ostalo"
+  ],
+  "Rudarsko-Geološko-Građevinski fakultet": [
+    "Rudarstvo", "Geologija", "Građevinarstvo", "Geodezija", "Ostalo"
+  ],
+  "Mašinski fakultet": [
+    "Mašinstvo", "Industrijsko inženjerstvo", "Ostalo"
+  ],
+  "Fakultet za tjelesni odgoj i sport": [
+    "Tjelesni odgoj i sport", "Ostalo"
+  ],
+  "Tehnološki fakultet": [
+    "Hemijsko inženjerstvo", "Prehrambena tehnologija", "Tekstilno inženjerstvo", "Ostalo"
+  ],
+  "Akademija dramskih umjetnosti": [
+    "Gluma", "Režija", "Dramaturgija", "Ostalo"
+  ],
+  "Edukacijsko-rehabilitacijski fakultet": [
+    "Logopedija i audiologija", "Specijalna edukacija", "Ostalo"
+  ],
+}
+
+export const FACULTY_NAMES = Object.keys(FACULTIES)
